b0404905c68e187ca846cde095281bb6ffc6e7c8
Franek Wojas <49079734+FranekDev@users.noreply.github.com>
2023-07-12 22:27:49 +0200

    Initial commit

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('test test');
+console.log('123');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0EsbUIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZXN0YXVyYW50LXBhZ2UvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ3Rlc3QgdGVzdCcpO1xyXG5jb25zb2xlLmxvZygnMTIzJyk7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
